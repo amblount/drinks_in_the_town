36c28117134a05b66ca3502038993c6bffc4f706
amblount <amblount@ucdavis.edu>
2015-09-28 19:47:45 -0700

    devise user authentication

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,3 +1,31 @@
 class NeighborhoodsController < ApplicationController
-  
+  def new
+    @neighborhood = Neighborhood.new
+  end
+
+  def create
+    @neighborhood = Neighborhood.new(neighborhood_params)
+    if @neighborhood.save
+      redirect_to neighborhoods.path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @neighborhood = get_neighborhood
+    @neighborhood.destroy
+    redirect_to neighborhoods_path
+  end
+
+  private
+
+  def get_neighborhood
+    Neighborhood.find(params[:id])
+  end
+
+  def neighborhood_params
+  end
+
+
 end
